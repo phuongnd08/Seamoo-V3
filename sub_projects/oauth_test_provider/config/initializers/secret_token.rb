63e1da7d1492f95d5f01b51da8315a8fb1507330
Phuong Nguyen <phuongnd08@gmail.com>
2010-11-28 10:22:08 +0700

    Restruct seamoo. Web is now the root folder

---
+++ sub_projects/oauth_test_provider/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OauthTestProvider::Application.config.secret_token = 'e6cf10c46d78d74b2b71b889a8012d691104b8537b7fb9914610cc62d6a6e5c938bf1f084bedacfa56b45c26c054eb194a5befb04423c0d7097389c0b377116d'
