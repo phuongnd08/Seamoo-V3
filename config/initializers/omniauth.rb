c2e436acb436e7590532b55120c53c05a713f769
Phuong Nguyen <phuongnd08@gmail.com>
2011-03-07 23:19:05 +0700

    [maintain] Change the storage option of open id to nil. Damn Heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,12 +1,10 @@
 #config/initializers/omniauth.rb
-require 'openid/store/filesystem'
 OAUTH =  YAML::load(ERB.new(IO.read(File.join(Rails.root, 'config', 'oauth.yml'))).result)[Rails.env]
 
 # you will be able to access the above providers by the following url
 # /auth/providername for example /auth/twitter /auth/facebook
 
-Rails.application.config.middleware.use OmniAuth::Strategies::OpenID, 
-  OpenID::Store::Filesystem.new(File.join(Rails.root, '/tmp')),
+Rails.application.config.middleware.use OmniAuth::Strategies::OpenID, nil,
   :name => "google",  :identifier => "https://www.google.com/accounts/o8/id"
   #use OmniAuth::Strategies::OpenID, OpenID::Store::Filesystem.new('/tmp'), :name => "yahoo",   :identifier => "https://me.yahoo.com"
   #use OmniAuth::Strategies::OpenID, OpenID::Store::Filesystem.new('/tmp'), :name => "aol",     :identifier => "https://openid.aol.com"
@@ -17,7 +15,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :facebook, OAUTH['facebook']['app_id'], OAUTH['facebook']['app_secret']
   # provider :twitter,  'KEY', 'SECRET'
   # provider :linked_in, 'KEY', 'SECRET'
-  provider :open_id,  OpenID::Store::Filesystem.new(File.join(Rails.root, '/tmp'))
+  provider :open_id,  nil
 end
 
 # you won't be able to access the openid urls like /auth/google
