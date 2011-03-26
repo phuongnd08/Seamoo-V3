28534f564bf263693888f4ca3e392459c62d672d
Phuong Nguyen <phuongnd08@gmail.com>
2011-03-26 12:08:47 +0700

    Modify compass setting so that it can run on heroku

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -3,4 +3,16 @@ require 'compass/app_integration/rails'
 require 'susy'
 require 'compass-colors'
 require 'fancy-buttons'
+
+# patch compass so that it will run on Heroku
+require 'fileutils'
+FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets"))
+
 Compass::AppIntegration::Rails.initialize!
+
+Rails.configuration.middleware.delete('Sass::Plugin::Rack')
+Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Sass::Plugin::Rack')
+
+Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
+    :urls => ['/stylesheets'],
+    :root => "#{Rails.root}/tmp")
