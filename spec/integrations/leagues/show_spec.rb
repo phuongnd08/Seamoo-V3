3104ed283c498c7098a430e5b1f2da31267cd7f9
Phuong Nguyen <phuongnd08@gmail.com>
2011-04-09 21:32:32 +0700

    Add history and authorization into shoutbox

--- spec/integrations/leagues/show_spec.rb
+++ spec/integrations/leagues/show_spec.rb
@@ -32,9 +32,36 @@ describe "League show page" do
     end
   end
 
+  describe "chatting" do
+    describe "for unauthorized user" do
+      it "should ask user to login inorder to chat" do
+        visit league_path(@league)
+        within(".chatting") do
+          page.should have_no_css("#chat_form")
+          page.should have_content("Please signin to chat")
+          path_of(page.find_link("signin")[:href]).should == signin_path
+        end
+      end
+    end
+    describe "for authorized user" do
+      before(:each) do
+        @user = Factory(:user, :display_name => "user")
+        Informer.login_as = "user"
+        visit league_path(@league)
+      end
+
+      it "should allow user to send chat" do
+        within(".chatting") do
+          page.should have_css("#chat_form")
+        end
+      end
+    end
+  end
+
   describe "page 2" do
+    before(:each){ visit league_path(@league, :page => 2)}
+
     it "should show list of next-good-performance users" do
-      visit league_path(@league, :page => 2)
       @users[0..4].reverse.each do |user|
         page.should have_content(user.display_name)
       end    
