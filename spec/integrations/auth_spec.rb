ef2257ac979e72e70a0861469d8cfdf718bd2fb9
Phuong Nguyen <phuongnd08@gmail.com>
2011-05-23 22:07:55 +0700

    Add yahoo login

--- spec/integrations/auth_spec.rb
+++ spec/integrations/auth_spec.rb
@@ -1,15 +1,60 @@
 require 'spec_helper'
-describe "Authorization" do
-  before(:each) do
-    OmniAuth.config.test_mode = true
-  end
-
-  after(:each) do
-    OmniAuth.config.test_mode = false
-  end
-  describe "using Google" do
+shared_examples_for "authorize using external provider" do
     before(:each) do
-      OmniAuth.config.mock_auth[:open_id] = @hash = {
+      OmniAuth.config.test_mode = true
+      OmniAuth.config.mock_auth[provider] = omniauth_hash 
+      visit signin_path
+    end
+
+    after(:each) do
+      OmniAuth.config.test_mode = false
+    end
+
+    describe "First Time" do
+      it "should welcome user for first time and persist authorization hash" do
+        click_link(link_text)
+        page.find_link(user_display_name).should_not be_nil
+        page.should have_content("Welcome to dautri.net, #{user_display_name}")
+        user = User.find_by_display_name_and_email(user_display_name, user_email)
+        user.should_not be_nil
+        user.authorizations.first.omniauth.should == omniauth_hash
+        if birthday.present?
+          user.date_of_birth.should == birthday
+        end
+      end
+    end
+
+    describe "Second Time" do
+      it "should welcome user back" do
+        User.create(:email => user_email, :display_name => user_display_name).
+          authorizations.create(:provider => provider.to_s, :uid => uid)
+        click_link(link_text)
+        page.should have_content("Welcome back, #{user_display_name}")
+      end
+    end
+
+    describe "Using emails that already exists" do
+      it "should notify user gratefully" do
+        User.create(:email => user_email, :display_name => user_display_name).
+          authorizations.create(:provider => "some_provider", :uid => "some uid")
+        click_link(link_text)
+        page.should have_content("Cannot log you in because email #{user_email} is already registered for another user. Perhaps it's just you, but in different service (Facebook/Google). Please try again")
+        current_path.should == signin_path
+      end
+    end
+
+end
+
+
+describe "Authorization" do
+  describe "using Google" do
+    let(:provider){:open_id}
+    let(:uid){"http://google.com/openid?id=test"}
+    let(:user_email){"test@google.com"}
+    let(:user_display_name){"Test User"}
+    let(:link_text){"Google"}
+    let(:omniauth_hash){
+      {
         "provider" => "open_id",
         "uid" => "http://google.com/openid?id=test",
         "user_info" => {
@@ -17,44 +62,21 @@ describe "Authorization" do
           "name"=> "Test User"
         }
       }
-      visit signin_path
-    end
+    }
+    let(:birthday){nil}
 
-    describe "First Time" do
-      it "should welcome user for first time and persist authorization hash" do
-        click_link("Google")
-        page.find_link("Test User").should_not be_nil
-        page.should have_content("Welcome to dautri.net, Test User")
-        user = User.find_by_display_name_and_email("Test User", "test@google.com")
-        user.should_not be_nil
-        user.authorizations.first.omniauth.should == @hash
-      end
-    end
-
-    describe "Second Time" do
-      it "should welcome user back" do
-        User.create(:email => "test@google.com", :display_name => "Test User").
-          authorizations.create(:provider => "open_id", :uid => "http://google.com/openid?id=test")
-        click_link("Google")
-        page.should have_content("Welcome back, Test User")
-      end
-    end
-
-    describe "Using emails that already exists" do
-      it "should notify user gratefully" do
-        User.create(:email => "test@google.com", :display_name => "Test_User").
-          authorizations.create(:provider => "some_provider", :uid => "some uid")
-        click_link("Google")
-        page.should have_content("Cannot log you in because email test@google.com is already registered for another user. Perhaps it's just you, but in different service (Facebook/Google). Please try again")
-        current_path.should == signin_path
-      end
-    end
+    it_should_behave_like "authorize using external provider"
   end
 
   describe "using Facebook" do
-    before(:each) do
-      OmniAuth.config.mock_auth[:facebook] = @hash = {
-        "user_info"=>{
+    let(:provider){:facebook}
+    let(:uid){"http://www.facebook.com/test"}
+    let(:user_email){"test@facebook.com"}
+    let(:user_display_name){"Test User"}
+    let(:link_text){"Facebook"}
+    let(:omniauth_hash){
+      {
+      "user_info"=>{
           "name" => "Test User",
           "email" => "test@facebook.com",
           "urls" => {"Facebook"=>"http://www.facebook.com/test", "Website"=>nil},
@@ -68,29 +90,31 @@ describe "Authorization" do
         "uid" => "http://www.facebook.com/test",
         "provider" => "facebook"
       }
+    }
+    let(:birthday){Date.new(1990, 01, 01)}
 
-      visit signin_path
-    end
+    it_should_behave_like "authorize using external provider"
+  end
 
-    describe "First Time" do
-      it "should welcome user for first time and save authorization hash" do
-        click_link("Facebook")
-        page.find_link("Test User").should_not be_nil
-        page.should have_content("Welcome to dautri.net, Test User")
-        user = User.find_by_display_name_and_email("Test User", "test@facebook.com")
-        user.should_not be_nil
-        user.date_of_birth.should == Date.new(1990, 1, 1)
-        user.authorizations.first.omniauth.should == @hash
-      end
-    end
+  describe "using Yahoo" do
+    let(:provider){:open_id}
+    let(:uid){"https://me.yahoo.com/test"}
+    let(:user_email){"test@yahoo.com"}
+    let(:user_display_name){"Test User"}
+    let(:link_text){"Yahoo"}
+    let(:omniauth_hash){
+      {
+        "user_info" => {
+          "name" => "Test User",
+          "nickname" => "Mr Test", 
+          "email" => "test@yahoo.com"
+        }, 
+        "uid" => "https://me.yahoo.com/test", 
+        "provider" => "open_id"
+      }
+    }
+    let(:birthday){nil}
 
-    describe "Second Time" do
-      it "should welcome user back" do
-        User.create(:email => "test@facebook.com", :display_name => "Test User").
-          authorizations.create(:provider => "facebook", :uid => "http://www.facebook.com/test")
-        click_link("Facebook")
-        page.should have_content("Welcome back, Test User")
-      end
-    end
+    it_should_behave_like "authorize using external provider"
   end
 end
